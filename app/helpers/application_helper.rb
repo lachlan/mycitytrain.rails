3e7b2e0de66316cca120058ba8910422a7b42860
dogeth <readkev@gmail.com>
2009-01-07 16:38:48 +1000

    Changed the implementation of format_time.  Now default is :short_html, and added :weekday option

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-	def format_time(time, format = :short)
+	def format_time(time, format = :short_html)
 	
 		case format
-		when :short then time.strftime('%I:%M %p').downcase
+		when :short_html then time.strftime('%I:%M&nbsp;%p').downcase
 		when :iso8601 then time.iso8601 
+		when :weekday then time.strftime('%A')
 		end
 		
 	end
-	
+
 end
