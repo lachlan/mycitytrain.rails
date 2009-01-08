aed6727ec0180491a34965fd5c11e5c68ebdd3a0
lachlan <lachlan.dowding@gmail.com>
2009-01-08 22:07:11 +1000

    Added rake task to purge old journeys and stops from the database.

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -7,7 +7,18 @@ class Journey < ActiveRecord::Base
   
   validates_presence_of :departing, :arriving, :departing_at
 
-  named_scope :departing_when, lambda { |start_date, end_date| end_date ? {:conditions => ['departing_at between ? and ?', start_date, end_date], :order => 'departing_at' } : { :conditions => ['departing_at > ?', start_date], :order => 'departing_at' } }
+  named_scope :departing_when, lambda { |start_date, end_date| 
+    if start_date and end_date
+      {:conditions => ['departing_at between ? and ?', start_date, end_date], :order => 'departing_at' }
+    elsif start_date
+      { :conditions => ['departing_at > ?', start_date], :order => 'departing_at' }
+    elsif end_date  
+      { :conditions => ['departing_at < ?', end_date], :order => 'departing_at' }
+    else
+      { :order => 'departing_at' } 
+    end
+  }
+  
   named_scope :departing_from, lambda { |station| { :conditions => ['departing_id = ?', station] }}
   named_scope :arriving_to, lambda { |station| { :conditions => ['arriving_id = ?', station] }}
   named_scope :limit, lambda { |limit| { :limit => limit }}
@@ -40,45 +51,45 @@ class Journey < ActiveRecord::Base
   end
   
   def self.upcoming(d, a)
-	self.fetch_journeys(:departing =>d, :arriving =>a, :from =>Time.zone.now, :limit =>10)
+    self.fetch_journeys(:departing => d, :arriving => a, :from => Time.zone.now, :limit => 10)
   end
   
   def self.today(d, a)
   	today = Time.zone.now.midnight 
-	self.fetch_journeys(:departing =>d, :arriving =>a, :from => today, :to => today + 1.day)
+    self.fetch_journeys(:departing => d, :arriving => a, :from => today, :to => today + 1.day)
   end
   
   def self.tomorrow(d, a)
   	tomorrow = Time.zone.now.tomorrow.midnight
-	self.fetch_journeys(:departing =>d, :arriving =>a, :from =>tomorrow, :to => tomorrow + 1.day)
+    self.fetch_journeys(:departing => d, :arriving => a, :from => tomorrow, :to => tomorrow + 1.day)
   end
   
   def self.fetch_journeys(o)
   	departing, arriving, from, to, limit = o[:departing], o[:arriving], o[:from], o[:to], o[:limit]
-	limit ||= 9999
+    limit ||= 9999
 	
   	journeys = Journey.departing_from(departing).arriving_to(arriving).departing_when(from, to).limit(limit)
 
-	retries = 0
+    retries = 0
     while (retries < 10 and (!journeys or journeys.length == 0))
       0.upto(1) { |i| CitytrainAPI.journeys(departing, arriving, Time.zone.now.midnight + i.day) }
-	  journeys = Journey.departing_from(departing).arriving_to(arriving).departing_when(from, to).limit(limit)
-	  retries += 1
-	  sleep 3 if retries > 1 #Sleep in between attempts (3 seconds)
-	end
+      journeys = Journey.departing_from(departing).arriving_to(arriving).departing_when(from, to).limit(limit)
+      retries += 1
+      sleep 3 if retries > 1 #Sleep in between attempts (3 seconds)
+    end
 	  		
-	journeys
+    journeys
   end
   
   alias base_stops stops
   def stops
-  	s = base_stops
-	retries = 0
+    s = base_stops
+    retries = 0
   	while (retries < 10 and s.empty?)
   		CitytrainAPI.stops self
   		reload
   		s = base_stops
-  	    retries += 1
+      retries += 1
 	    sleep 3 if retries > 1 #Sleep in between attempts (3 seconds)
   	end
   	s
---
+++ lib/tasks/mycitytrain.rake
@@ -0,0 +1,19 @@
+namespace :mycitytrain do
+  namespace :db do
+    desc "Destroys all journeys and stops older than a day"
+    task :purge => :environment do
+      condition = ["departing_at < ?", Time.zone.now.yesterday.midnight]
+      Stop.destroy_all condition
+      Journey.destroy_all condition
+    end
+    
+    desc "Loads stations, journeys and stops from Citytrain API"
+    task :populate => :environment do
+      # Load stations
+      CitytrainAPI.stations
+      
+      # TODO: Load future journeys for any journeys that already exist
+      # TODO: Load future stops for any journeys that already exist
+    end
+  end
+end
