9da6dd0011cf3c76596a38fa3f66b029b3e1b6b1
Kevin Read <readkev@gmail.com>
2009-01-13 13:46:54 +1000

    Added after_update_code task to the capistrano deploy (not yet tested)

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,5 +12,13 @@ set :user, "train"
 ssh_options[:port] = 3636
 set :use_sudo, false
 
+namespace :deploy do
+  #See http://toolmantim.com/articles/setting_up_capistrano_on_segpub  
+   desc "Link in the production database.yml"
+   task :after_update_code do
+     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+   end
+end
+
 
 
