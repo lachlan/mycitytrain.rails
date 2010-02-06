fa069e5d70696d76cad4cc8afd19120da0dbf6b8
lachlan <lachlan.dowding@gmail.com>
2010-02-06 15:57:00 +1000

    Fixed display of journey stops to work when arriving_at is missing on journey

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,9 +10,13 @@ module ApplicationHelper
 	end
 	
 	def duration_in_words(from_time, to_time)
-    from_time = from_time.to_time if from_time.respond_to?(:to_time)
-    to_time = to_time.to_time if to_time.respond_to?(:to_time)
-    duration_in_seconds = ((to_time - from_time).abs).round
+    if from_time and to_time
+      from_time = from_time.to_time if from_time.respond_to?(:to_time)
+      to_time = to_time.to_time if to_time.respond_to?(:to_time)
+      duration_in_seconds = ((to_time - from_time).abs).round
+    else
+      duration_in_seconds = 0
+    end
     
     case duration_in_seconds
       # between 0 seconds and 1 minute
