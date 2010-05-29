86edec6c39b17bd89949c5cf92251100c658b8d6
Kevin Read <readkev@gmail.com>
2010-05-29 15:55:07 +1000

    fixed has_many problem

--- app/models/timetable_type.rb
+++ app/models/timetable_type.rb
@@ -1,5 +1,6 @@
 class TimetableType < ActiveRecord::Base
-  has_many :timetable_days, :journeys  
+  has_many :timetable_days
+  has_many :journeys  
   
   validates_presence_of :name
   validates_uniqueness_of :name
