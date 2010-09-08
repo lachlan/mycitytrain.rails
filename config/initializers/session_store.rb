3a4ad5da9914b8aa1e34eedaef813f783e8249d2
lachlan <lachlan.dowding@gmail.com>
2010-09-08 22:22:05 +1000

    Fix session expiry to be far into the future, so favourites are kept forever

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MyCitytrain::Application.config.session_store :cookie_store, :key => '_my_citytrain_session'
+MyCitytrain::Application.config.session_store :cookie_store, :key => '_my_citytrain_session', :expire_after => 25.years
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
