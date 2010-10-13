d09e7d3a25f25088c277cca62fdaf8fbbadcc0b3
lachlan <lachlan.dowding@gmail.com>
2010-10-13 18:28:38 +1000

    Start cleanup of javascript, ETAs now updated properly after changing favourites via form

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,117 +1,73 @@
-// crappy pluralize implementation
-function plural(singular) {
-  return singular + "s";
-}
+// Generates an id suitable for use in HTML: http://www.w3.org/TR/html4/types.html#type-name
+String.prototype.id = function() { return 'id_' + this.toString().replace(/[^a-zA-Z0-9]/g, '_'); }
 
-function pluralize(count, singular) {
-  var p = (count == 1 || count == '1')? singular : plural(singular);
-  if (count) 
-    p = count + " " + p;
-  else
-    p = "0 " + p;
-  return p;
-}
-
-function durationInSeconds(fromDate, toDate) {
-  return Math.round((toDate - fromDate)/1000);
-}
-
-function durationInWords(durationInSeconds) {
-  var duration = durationInSeconds;
-  var phrase;
-  var unsigned_duration = duration;
-  if (duration < 0) {
-    unsigned_duration = duration * -1;
+// Pluralizes a string by adding an 's' on the end
+String.prototype.pluralize = function(count) {
+  var plural = function(singular) {
+    return singular + "s";
   }
-  
-  if (unsigned_duration == 0) {
-    phrase = "now";
-  } else if (unsigned_duration >= 1 && unsigned_duration <= 3599) {
-    phrase = pluralize(Math.ceil(duration/60), "min");
-  } else if (unsigned_duration >= 3600 && unsigned_duration <= 86399) {
-    var durationInHours = Math.floor(duration/3600);
-    var remainderInMinutes = (duration % 3600)/60;
-    if (remainderInMinutes < 30)
-      phrase = pluralize(durationInHours, "hour");
-    else
-      phrase = pluralize(durationInHours + 0.5, "hour");
-  } else if (unsigned_duration >= 86400 && unsigned_duration <= 2591999) {
-    var durationInDays = Math.floor(duration/86400);
-    var remainderInHours = (duration % 86400)/3600;
-    if (remainderInHours < 12)
-      phrase = pluralize(durationInDays, "day");
-    else
-      phrase = pluralize(durationInDays + 0.5, "day");
-  } else if (unsigned_duration <= 2592000 && unsigned_duration <= 31535999) {
-    phrase = pluralize(Math.ceil(duration/2592000), "mth");
-  } else {
-    phrase = pluralize(Math.ceil(duration/31536000), "yr");
-  }
-  return phrase;
-}
-
-var millisecondsUntilNextMinute = function() {
-  var now = new Date();
-  var next = new Date();
-  next.setMinutes(now.getMinutes() + 1);
-  next.setSeconds(0);
-  next.setMilliseconds(0);
-  return next - now;
-}
-
-var generateID = function(href) {
-  return 'id' + href.replace(/[^a-zA-Z0-9]/g, '_');
-}
-
-var locations = new Array();
-var search = function(term, exact) {
-  if (exact)
-    var pattern = new RegExp('^' + term + '$', 'i');
-  else
-    var pattern = new RegExp('^' + term, 'i');
-  var matches = new Array();
-  for (var i = 0; i < locations.length; i++) {
-    if (locations[i].match(pattern)) matches.push(locations[i]);
-  }
-  return matches;
+  if (count == undefined) count = 0;
+  return count + " " + ((count == 1 || count == '1')? this : plural(this));
 };
 
-var loadLocations = function(callback) {
-  if (locations.length == 0) {
-	  $.getJSON('/locations', function(data) {
-	    locations = data;
-      if (callback) callback();
-    });
-  } else {
-    if (callback) callback();
-  }
-};
-
-var updateLocationName = function(element) {
-  var value = $(element).attr('value');
-  if (value) {
-    var matches = search(value, true);
-    if (matches.length > 0) {
-      $(element).attr('value', matches[0]);
+// Returns the duration between two dates
+Date.prototype.duration = function(other, unit) {
+  var duration = other - this;
+  if (unit == 'seconds') {
+    duration = Math.round(duration/1000); 
+  } else if (unit == 'minutes') {
+    duration = Math.round(duration/(60000));
+  } else if (unit == 'words') {
+    duration = Math.round(duration/1000);
+    if (duration <= 0) {
+      duration = "now";
+    } else if (duration >= 1 && duration <= 3599) {
+      duration = "min".pluralize(Math.ceil(duration/60));
+    } else if (duration >= 3600 && duration <= 86399) {
+      var durationInHours = Math.floor(duration/3600);
+      var remainderInMinutes = (duration % 3600)/60;
+      if (remainderInMinutes < 30) {
+        duration = "hour".pluralize(durationInHours);
+      } else {
+        duration = "hour".pluralize(durationInHours + 0.5);
+      }
+    } else if (duration >= 86400 && duration <= 2591999) {
+      var durationInDays = Math.floor(duration/86400);
+      var remainderInHours = (duration % 86400)/3600;
+      if (remainderInHours < 12) {
+        duration = "day".pluralize(durationInDays);
+      } else {
+        duration = "day".pluralize(durationInDays + 0.5);
+      }
+    } else if (duration <= 2592000 && duration <= 31535999) {
+      duration = "mth".pluralize(Math.ceil(duration/2592000));
+    } else {
+      duration = "yr".pluralize(Math.ceil(duration/31536000));
     }
   }
+  return duration;
 };
 
-// save the original value of each form input field
-// only works for <input> elements
-var saveFormValues = function(form) {
-  $(form).find('input').each(function() {
-    $(this).data('value', $(this).attr('value'));
-  });
+// Returns a Date for the next second, minute or hour
+Date.prototype.next = function(unit) {
+  var n = new Date(this.getTime());
+  if (unit == 'millisecond' || unit == undefined) {
+    n.setMilliseconds(this.getMilliseconds() + 1);
+  } else if (unit == 'second') {
+    n.setSeconds(this.getSeconds() + 1);
+    n.setMilliseconds(0);
+  } else if (unit == 'minute') {
+    n.setMinutes(this.getMinutes() + 1);
+    n.setSeconds(0);
+    n.setMilliseconds(0);
+  } else if (unit == 'hour') {
+    n.setHours(this.getHours() + 1);
+    n.setMinutes(0);
+    n.setSeconds(0);
+    n.setMilliseconds(0);
+  }
+  return n;
 }
-
-var formHasChanged = function(form) {
-  var dirty = false;
-  $(form).find('input').each(function() {
-    dirty = dirty || ($(this).attr('value') !== $(this).data('value'));
-  });
-  return dirty;
-};
 
 $(document).ready(function() {
   $.support.WebKitAnimationEvent = (typeof WebKitTransitionEvent == "object");
@@ -119,6 +75,57 @@ $(document).ready(function() {
   var journey_limit = 5;
   var active = 'active';
   var effects = 'fx in out flip slide pop cube swap slideup dissolve fade reverse';
+  
+  var locations = new Array();
+  var search = function(term, exact) {
+    if (exact)
+      var pattern = new RegExp('^' + term + '$', 'i');
+    else
+      var pattern = new RegExp('^' + term, 'i');
+    var matches = new Array();
+    for (var i = 0; i < locations.length; i++) {
+      if (locations[i].match(pattern)) matches.push(locations[i]);
+    }
+    return matches;
+  };
+
+  var loadLocations = function(callback) {
+    if (locations.length == 0) {
+  	  $.getJSON('/locations', function(data) {
+  	    locations = data;
+        if (callback) callback();
+      });
+    } else {
+      if (callback) callback();
+    }
+  };
+
+  var updateLocationName = function(element) {
+    var value = $(element).attr('value');
+    if (value) {
+      var matches = search(value, true);
+      if (matches.length > 0) {
+        $(element).attr('value', matches[0]);
+      }
+    }
+  };
+
+  // save the original value of each form input field
+  // only works for <input> elements
+  var saveFormValues = function(form) {
+    $(form).find('input').each(function() {
+      $(this).data('value', $(this).attr('value'));
+    });
+  }
+
+  var formHasChanged = function(form) {
+    var dirty = false;
+    $(form).find('input').each(function() {
+      dirty = dirty || ($(this).attr('value') !== $(this).data('value'));
+    });
+    return dirty;
+  };
+  
 
   // set up the autocomplete suggestions on the form input fields
 	$('input.origin, input.destination').autocomplete({
@@ -168,16 +175,16 @@ $(document).ready(function() {
         setActivePage(setMinHeight);
       }
       if ($.support.WebKitAnimationEvent) {
-        linkee.one('webkitAnimationEnd', function() { animationFinished(); });
+        linkee.one('webkitAnimationEnd', animationFinished);
       } else {
         animationFinished();
       }
-    }    
-    if ((href).match('^#')) {
+    }
+    if (href.match('^#')) {
       fx($(href));
     } else {
       // load external links via ajax directly into page content
-      var id = generateID(href);
+      var id = href.id();
       $('body').find('#' + id).remove();
       $('body').append('<div id="' + id + '" class="page"></div>');
       link.addClass('disabled');
@@ -210,6 +217,7 @@ $(document).ready(function() {
         $.post(form.attr('action'), form.serialize(), function(data) {
           saveFormValues(form);
           loadFavourites(function() {
+            updateETAs();
             if ($('#favourites').length > 0) {
               link.trigger('transition').removeClass('disabled');
             }
@@ -255,7 +263,7 @@ $(document).ready(function() {
       if (limit && limit > 0) href = href + '&limit=' + limit;
 
       $.get(href, function(data) {
-        var id = generateID(href);
+        var id = unescape(href).id() ;
         $('#' + id).remove();
         $('body').append('<div id="' + id + '"></div>');
         $('#' + id).append(data);
@@ -294,63 +302,52 @@ $(document).ready(function() {
   };
   $(window).resize(setMinHeight);
   
-  // keep the ETAs up to date with current time and remove departed journeys
+  // keep the ETAs up to date with current time
   var updateETAs = function() {
     $('.journey:not(.missing)').each(function() {
       var journey = $(this);
       var eta = journey.find('.eta');
-      var departureDate = new Date();
-      var currentDate = new Date();
-      departureDate.setTime(journey.attr('data-depart-time-js'));
+      var departure = new Date();
+      var now = new Date();
+      departure.setTime(journey.attr('data-depart-time-js'));
     
-      var duration = durationInSeconds(currentDate, departureDate);
-    
-      var durationInMinutes = duration/60;
-      if (durationInMinutes <= 5) {
-        eta.removeClass("lt_ten").addClass("lt_five");
-      } else if (durationInMinutes <= 10) {
-        eta.addClass("lt_ten");
-      }
-    
-      if (duration > 0 || journey.hasClass('detail')) {
-        eta.html(durationInWords(duration));
-      } else {
-        eta.html("now");
-        journey.addClass('expired');
-      }
+      var minutes = now.duration(departure, 'minutes');
+      if (minutes <= 10) eta.addClass('soon');
+      if (minutes <= 5) eta.addClass('now');
+      eta.html(now.duration(departure, 'words'));
+      if (now.duration(departure, 'seconds') <= 0) journey.addClass('expired');
     });
-    
-    var flasher = function(element) {
-      if (element) {
-        element.animate({opacity:0.5},1000,'linear',function() {
-          $(this).animate({opacity:1},1000,'linear',function() {
-            $(this).animate({opacity:0.5},1000,'linear',function() {
-              $(this).animate({opacity:1},1000,'linear',function() {
-                $(this).animate({opacity:0.5},1000,'linear',function() {
-                  $(this).animate({opacity:1},1000,'linear',function() {                        
-                    $(this).slideUp('slow', function() {
-                      var parent = $(this).parents('.journeys');
-                      $(this).remove();
-                    });
+  }
+  
+  var flasher = function(element) {
+    if (element) {
+      element.animate({opacity:0.5},1000,'linear',function() {
+        $(this).animate({opacity:1},1000,'linear',function() {
+          $(this).animate({opacity:0.5},1000,'linear',function() {
+            $(this).animate({opacity:1},1000,'linear',function() {
+              $(this).animate({opacity:0.5},1000,'linear',function() {
+                $(this).animate({opacity:1},1000,'linear',function() {                        
+                  $(this).slideUp('slow', function() {
+                    var parent = $(this).parents('.journeys');
+                    $(this).remove();
                   });
                 });
               });
             });
           });
         });
-      }
-    };
+      });
+    }
+  };
   
+  var expireDepartedJourneys = function() {
     $('.journeys').each(function() {
       var journeys = $(this);
       var expired = journeys.find('.journey.expired');
       var count = journey_limit - (journeys.find('.journey:not(.missing)').length - expired.length);
-      if (count > 0) loadMoreJourneys(journeys, count);
+      if (count > 0) loadMoreJourneys(journeys, count, updateETAs);
       flasher(expired);
-    });
-
-    // reschedule next run
-    window.setTimeout(updateETAs, millisecondsUntilNextMinute());
+    });    
   }
   
   var browserWarning = $('.ie6 .browser-warning, .ie7 .browser-warning, .ie8 .browser-warning');
@@ -363,5 +360,12 @@ $(document).ready(function() {
   })
   
   setActivePage(setMinHeight);
-  updateETAs();
+
+  (function runner() {
+    updateETAs();
+    expireDepartedJourneys();
+    // schedule next run
+    var now = new Date();
+    window.setTimeout(runner, now.duration(now.next('minute')));
+  })();
 });
