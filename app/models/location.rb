b63d8f3a88a1d38eaee962acdc0df94c57e2a8bc
lachlan <lachlan.dowding@gmail.com>
2011-05-23 16:43:11 +1000

    Whitespace changes to Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,10 +5,10 @@ class Location < ActiveRecord::Base
   include Comparable
 
   has_many :journeys, :finder_sql => 'SELECT journeys.* FROM journeys WHERE journeys.origin_id = #{id} OR journeys.destination_id = #{id}'
-  
+
   validates_presence_of :name
   validates_uniqueness_of :name
-  
+
   # Finds a location given an ID or name.
   #
   # Returns the matching Location.
@@ -18,7 +18,7 @@ class Location < ActiveRecord::Base
     location = where('name like ?', term).limit(1).first if location.nil?
     location
   end
-  
+
   # Downloads all CityTrain station Locations from the TransLink web site.
   #
   # Returns the Integer count of Locations downloaded.
@@ -27,16 +27,16 @@ class Location < ActiveRecord::Base
    locations = html.css('select option').map do |option|
      option['value'].strip
    end
-   
+
    locations.map { |location| Location.find_or_create_by_name(:name => location.gsub(/ Railway Station/, '').downcase) }
    locations.count
   end
-  
+
   # Redefine name attribute to be titleized.
   def name
     attributes['name'].titleize
   end
-  
+
   # Converts a Location name to a TransLink formatted CityTrain station name
   #
   # Returns a String that names a TransLink CityTrain station, that can be used
@@ -48,10 +48,10 @@ class Location < ActiveRecord::Base
       'Domestic Airport' => 'Airport Domestic',
       'International Airport' => 'Airport International'
     }
-    
+
     (aliases[name] || name).gsub(/'/, '').gsub(/ \(Vulture St\)/i, '') + " Railway Station"
   end
-  
+
   # Location spaceship operator: http://en.wikipedia.org/wiki/Spaceship_operator.
   #
   # other - Another Location for comparison with this Location.
@@ -61,7 +61,7 @@ class Location < ActiveRecord::Base
   def <=> (other)
     self.name <=> other.name
   end
-  
+
   # Returns a String containing the URL for getting a list of TransLink CityTrain stations.
   def self.url
     'http://www.queenslandrail.com.au/AllStations/Pages/AllStations.aspx'
